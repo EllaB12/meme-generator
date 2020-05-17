480a813801ef0693d77cb02b53cabb382c96f574
Ella <ella.brazlev@gmail.com>
2020-05-17 11:39:38 +0300

    fix search

--- js/meme-service.js
+++ js/meme-service.js
@@ -2,24 +2,24 @@
 
 var gKeywords = { 'happy': 12, 'funny puk': 1 };
 var gImgs = [
-    { id: 1, url: 'img/1.jpg', keywords: ['funny'] },
-    { id: 2, url: 'img/2.jpg', keywords: ['cute'] },
-    { id: 3, url: 'img/3.jpg', keywords: ['cute'] },
-    { id: 4, url: 'img/4.jpg', keywords: ['cute'] },
-    { id: 5, url: 'img/5.jpg', keywords: ['cute','happy'] },
-    { id: 6, url: 'img/6.jpg', keywords: ['cute'] },
-    { id: 7, url: 'img/7.jpg', keywords: ['cute','funny'] },
+    { id: 1, url: 'img/1.jpg', keywords: ['funny','politician'] },
+    { id: 2, url: 'img/2.jpg', keywords: ['cute','animal','dog'] },
+    { id: 3, url: 'img/3.jpg', keywords: ['cute','animal','baby','dog'] },
+    { id: 4, url: 'img/4.jpg', keywords: ['cute','animal','cat'] },
+    { id: 5, url: 'img/5.jpg', keywords: ['cute','happy','baby'] },
+    { id: 6, url: 'img/6.jpg', keywords: [] },
+    { id: 7, url: 'img/7.jpg', keywords: ['funny'] },
     { id: 8, url: 'img/8.jpg', keywords: ['cute'] },
-    { id: 9, url: 'img/9.jpg', keywords: ['cute', 'funny'] },
-    { id: 10, url: 'img/10.jpg', keywords: ['cute', 'funny'] },
-    { id: 11, url: 'img/11.jpg', keywords: ['cute'] },
-    { id: 12, url: 'img/12.jpg', keywords: ['cute'] },
-    { id: 13, url: 'img/13.jpg', keywords: ['cute'] },
-    { id: 14, url: 'img/14.jpg', keywords: ['cute'] },
-    { id: 15, url: 'img/15.jpg', keywords: ['cute'] },
-    { id: 16, url: 'img/16.jpg', keywords: ['cute'] },
-    { id: 17, url: 'img/17.jpg', keywords: ['cute'] },
-    { id: 18, url: 'img/18.jpg', keywords: ['cute'] },
+    { id: 9, url: 'img/9.jpg', keywords: ['funny'] },
+    { id: 10, url: 'img/10.jpg', keywords: ['funny','politician'] },
+    { id: 11, url: 'img/11.jpg', keywords: [] },
+    { id: 12, url: 'img/12.jpg', keywords: ['celebrity'] },
+    { id: 13, url: 'img/13.jpg', keywords: ['celebrity'] },
+    { id: 14, url: 'img/14.jpg', keywords: [] },
+    { id: 15, url: 'img/15.jpg', keywords: [] },
+    { id: 16, url: 'img/16.jpg', keywords: ['excited'] },
+    { id: 17, url: 'img/17.jpg', keywords: ['politician'] },
+    { id: 18, url: 'img/18.jpg', keywords: [] },
 ];
 var gMeme = creatMeme();
 var gFilter = 'All';
@@ -29,7 +29,7 @@ function getImgForDisplay() {
     if (gFilter === 'All' || !gFilter) return gImgs;
     addKeyWord(gFilter);
     var imgsForDisplay = gImgs.filter(function (img) {
-        return img.keywords.find(keyWord =>  keyWord.includes(gFilter))
+        return img.keywords.find(keyWord =>  keyWord.includes(gFilter.toLowerCase()))
     })
 
     return imgsForDisplay;
